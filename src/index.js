1f9851c06f781366066b9d657a99ecca2891cf9d
lshapz <laurashapiro01@gmail.com>
2016-11-22 13:18:47 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(<InsertTopLevelComponent />, document.getElementById('container'))
 
 require('../test/index-test.js'); // Leave this in!
+import { createStore } from './store';
+import BandInput from './components/band_input_component';
+
+const renderApp = () => {
+  ReactDOM.render(<BandInput store={store}/>, document.getElementById('container'))
+}
+ 
+store.subscribe(renderApp);
+store.dispatch({})
